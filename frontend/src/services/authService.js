e97bcf984374be16fafee06a8f67482904e32e45
Umer <umerawan19791@gmail.com>
2025-09-20 19:26:01 +0500

    updated

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth";
+const API_URL = "https://e-commerce-backend-production-cb1a.up.railway.app/api/auth";
+// const API_URL = "http://localhost:5000/api/auth";
 
 const loginUser = async (credentials) => {
   const response = await axios.post(`${API_URL}/login`, credentials);
